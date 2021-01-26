e59eef58bc79b77de02c71c46d45e2ca4ac59228
Alex <dovgaloleksandr388@gmail.com>
2021-01-26 18:07:08 +0200

    fix details

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ for(let i = 0; i < headerLink.length; i++){
 let one = document.querySelector('.one');
 let h_one = document.querySelector('.h1_one');
 
-h_one.addEventListener('click', function(event){
+h_one.addEventListener('click', function(){
     one.style.display = 'block';
     two.style.display = 'none';
     three.style.display = 'none';
@@ -31,7 +31,7 @@ h_one.addEventListener('click', function(event){
 let two = document.querySelector('.two');
 let h_two = document.querySelector('.h1_two');
 
-h_two.addEventListener('click', function(event){
+h_two.addEventListener('click', function(){
     two.style.display = 'block';
     one.style.display = 'none';
     three.style.display = 'none';
@@ -41,7 +41,7 @@ h_two.addEventListener('click', function(event){
 let three = document.querySelector('.three');
 let h_three = document.querySelector('.h1_three');
 
-h_three.addEventListener('click', function(event){
+h_three.addEventListener('click', function(){
     three.style.display = 'block';
     two.style.display = 'none';
     one.style.display = 'none';
