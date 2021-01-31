64a3425734a192679981c563e5affe7f4e9eb14b
Alex <dovgaloleksandr388@gmail.com>
2021-01-31 23:07:04 +0200

    fixed news

--- main.js
+++ main.js
@@ -17,32 +17,3 @@ for(let i = 0; i < headerLink.length; i++){
         headerBurger.classList.toggle('active');
     })
 }
-
-let one = document.querySelector('.one');
-let h_one = document.querySelector('.h1_one');
-
-h_one.addEventListener('click', function(){
-    one.style.display = 'block';
-    two.style.display = 'none';
-    three.style.display = 'none';
-})
-
-
-let two = document.querySelector('.two');
-let h_two = document.querySelector('.h1_two');
-
-h_two.addEventListener('click', function(){
-    two.style.display = 'block';
-    one.style.display = 'none';
-    three.style.display = 'none';
-})
-
-
-let three = document.querySelector('.three');
-let h_three = document.querySelector('.h1_three');
-
-h_three.addEventListener('click', function(){
-    three.style.display = 'block';
-    two.style.display = 'none';
-    one.style.display = 'none';
-})
